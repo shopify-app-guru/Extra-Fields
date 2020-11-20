1a6b708316c73012e63f6197e65ee629421cc31e
George Ma <georgema.creative@gmail.com>
2020-11-21 01:51:06 +0800

    add all controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Rails.application.routes.draw do
+  get '/products', :to => 'products#index'
   root :to => 'home#index'
+
+  resource :marketing_activities, only: [:create, :update] do
+    patch :resume
+    patch :pause
+    patch :delete
+    post :republish
+    post :preload_form_data
+    post :preview
+    post :errors
+  end
   mount ShopifyApp::Engine, at: '/'
+# frozen_string_literal: true
+
+namespace :app_proxy do
+  root action: 'index'
+  # simple routes without a specified controller will go to AppProxyController
+
+  # more complex routes will go to controllers in the AppProxy namespace
+  #   resources :reviews
+  # GET /app_proxy/reviews will now be routed to
+  # AppProxy::ReviewsController#index, for example
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
