77035a53249b210cc197d976361556417ac18ac4
George Ma <georgema.creative@gmail.com>
2020-11-21 05:17:53 +0800

    add test data

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -6,29 +6,22 @@ import {
 } from '@apollo/client';
 import { AppProvider, EmptyState, Page } from '@shopify/polaris';
 import { authenticatedFetch } from '@shopify/app-bridge-utils';
+import TestData from './TestData'
 
 import enTranslations from '@shopify/polaris/locales/en.json';
 import React from 'react';
 
 export default function App() {
-  const client = new ApolloClient({
-    link: new HttpLink({
-      credentials: 'same-origin',
-      fetch: authenticatedFetch(window.app), // created in shopify_app.js
-      uri: '/graphql'
-    }),
-    cache: new InMemoryCache()
-  });
 
   return (
-    <AppProvider i18n={enTranslations}>
-      <ApolloProvider client={client}>
-        <Page>
-          <EmptyState>
-            test
-          </EmptyState>
-        </Page>
-      </ApolloProvider>
-    </AppProvider>
-  );
-}
+     <AppProvider i18n={enTranslations}>
+       <ApolloProvider client={client}>
+         <Page>
+           <EmptyState>
+             <TestData/>    # Add the TestData component here
+           </EmptyState>
+         </Page>
+       </ApolloProvider>
+     </AppProvider>
+   );
+ }
